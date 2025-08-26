fdf8908e400f88249d2f34db1936a4d3fcf4bf35
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-08-25 22:14:36 -0300

    Funções matematicas finalizadas

---
+++ src/curso_progrmacaoJAVA/FuncoesMatematicas.java
@@ -0,0 +1,37 @@
+package curso_progrmacaoJAVA;
+
+public class FuncoesMatematicas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C;
+		
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25.0);
+		
+		System.out.println("Raiz quadrada de " +x+ " = " +A);
+		System.out.println("Raiz quadrada de " +y+ " = " +B);
+		System.out.println("Raiz quadrada de 25 = " +C);
+		
+		
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		
+		System.out.println(x + " elevado a "+y+ " = " +A);
+		System.out.println(x + " elevado a 2 = " +B);
+		System.out.println("5 elevado a 2 = " +C);
+		
+		A = Math.abs(y);
+		B = Math.abs(z);
+		System.out.println("O valor absoluto de "+y+ " = " +A);
+		System.out.println("O valor absoluto de "+z+ " = " +B);
+		
+	}
+
+}
