b4a76d9b774f3188dc4262808e2be6fb1cce3c66
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-09-08 21:25:49 -0300

    Iniciado estudo no For

---
+++ Orientacao_a_Objetos/src/Estrutura_Repetitiva/For.java
@@ -0,0 +1,16 @@
+package Estrutura_Repetitiva;
+
+import java.util.Iterator;
+
+public class For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 0; i<5; i++) {
+			System.out.println("Int é: " +i);
+		}
+
+	}
+
+}
