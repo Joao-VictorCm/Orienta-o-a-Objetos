2417f1dd656bd5698eba6e390900c20e11e25629
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-10-19 19:55:07 -0300

    Construtor

---
+++ Orientacao_a_Objetos/src/poo/Contrutorer.java
@@ -0,0 +1,11 @@
+package poo;
+
+public class Contrutorer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+https://www.notion.so/Construtor-28e419d84e5e80a6a106cfbdf2c6423d
