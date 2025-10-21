f7c2505e9e70153560f63ae6aa5b1a27dea18d95
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-10-20 21:42:00 -0300

    Construindo construtores

---
+++ Orientacao_a_Objetos/src/contructor/entities.java
@@ -0,0 +1,29 @@
+package contructor;
+
+public class entities {
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public Entities() {
+		this.name = name;
+	}
+
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+
+	public String toString() {
+		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
+				+ String.format("%.2f", totalValueInStock());
+	}
+
+}
